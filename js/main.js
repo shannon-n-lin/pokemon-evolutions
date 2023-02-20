c9ac3ab02eb03f6c4f04d1792d65f7fd4d3fd33d
Shannon Lin <shannon@Shannons-MacBook-Air.local>
2023-02-20 15:26:53 -0800

    Update evolution chain fetch

--- js/main.js
+++ js/main.js
@@ -4,32 +4,42 @@ function getFetch() {
     const choice = document.getElementById('choice').value.toLowerCase()
     const url = 'https://pokeapi.co/api/v2/pokemon-species/' + choice
 
-    fetch(url)
+    fetch(urlSpecies)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-
-            document.getElementById('choiceName').innerText = data.name[0].toUpperCase() + data.name.slice(1)
-
-            // document.getElementById('choiceImg').src = 
-            
-            fetch(data.evolution_chain.url)
-                .then(res => res.json()) // parse response as JSON
+            return data.evolution_chain.url})
+        .then(urlEvolution => {
+            fetch(urlEvolution)
+                .then(res => res.json()) 
                 .then(data => {
-                    
-                    // document.getElementById('evolvesToName').innerText = 
+                    let poke1 = data.chain.species.name
+                    let poke2 = data.chain.evolves_to[0].species.name
+                    let poke3 = data.chain.evolves_to[0].evolves_to[0].species.name
 
-                    console.log(data.chain.evolves_to[0].evolves_to[0].species.name)
+                    document.getElementById('poke1').innerText = poke1[0].toUpperCase() + poke1.slice(1)
+                    document.getElementById('poke2').innerText = poke2[0].toUpperCase() + poke2.slice(1)
+                    document.getElementById('poke3').innerText = poke3[0].toUpperCase() + poke3.slice(1)
 
+                    printSprite(poke1, 'poke1Img')
+                    printSprite(poke2, 'poke2Img')
+                    printSprite(poke3, 'poke3Img')
                 })
-
                 .catch(err => {
                     console.log(`error ${err}`)
-                })
-
+                })   
         })
-
         .catch(err => {
             console.log(`error ${err}`)
         })
 
+    function printSprite(pokemon, img) {
+        fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+            .then(res => res.json())
+            .then(data => {
+            document.getElementById(img).src = data.sprites.front_default
+            })
+            .catch(err => {
+            console.log(`error ${err}`)
+            })
+    }
 }
