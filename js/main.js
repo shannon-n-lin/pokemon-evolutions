af8cbbcc56274d198d846f4d4bfcf401640bb08f
Shannon Lin <shannon@Shannons-MacBook-Air.local>
2023-02-19 16:14:16 -0800

    Set up pokemon api

---
+++ js/main.js
@@ -0,0 +1,31 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+    const choice = document.getElementById('choice').value.toLowerCase()
+    const url = 'https://pokeapi.co/api/v2/pokemon-species/' + choice
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+
+            document.getElementById('choiceName').innerText = data.name[0].toUpperCase() + data.name.slice(1)
+
+            // document.getElementById('choiceImg').src = 
+
+            fetch(data.evolution_chain.url)
+                .then(res => res.json()) // parse response as JSON
+                .then(data => {
+                    
+                })
+
+                .catch(err => {
+                    console.log(`error ${err}`)
+                })
+
+        })
+
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+
+}
